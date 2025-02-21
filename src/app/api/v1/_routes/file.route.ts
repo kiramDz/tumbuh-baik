5dae1a06271331c71d1e67f4e5b6368115b79571
dzulkiram hilmi <bukutulis0110@gmail.com>
2025-02-21 10:11:12 +0700

    feat: Add lean, select, and sort to recent files filter

--- src/app/api/v1/_routes/file.route.ts
+++ src/app/api/v1/_routes/file.route.ts
@@ -90,6 +90,135 @@ fileRoute.get("/recent", async (c) => {
   }
 });
 
+fileRoute.post("/upload", async (c) => {
+  try {
+    // pastikan koneksi dengan database
+    await db();
+
+    const data = await c.req.formData();
+    // const file: File | null = data.get("file") as unknown as File;
+    // Ambil file dari request (data.get("file")).
+    const file = data.get("file") as File | null;
+    if (!file) {
+      return c.json({ message: "No file provided" }, { status: 400 });
+    }
+    console.log("Received file:", file);
+    console.log("File type:", file?.type);
+
+    const session = await getServerSession();
+
+    if (!session) {
+      return c.json(
+        {
+          file: null,
+          message: "Unauthorized",
+          description: "You need to be logged in to upload files",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    const userId = session.user.id;
+    const name = session.user.name;
+
+    const subs = await Subscription.findOne({ subscriber: userId });
+
+    if (!subs) {
+      return c.json(
+        {
+          message: "⚠️ Warning",
+          category: null,
+          description: "Subscription not found. Please log out and log in again to refresh your session.",
+          file: null,
+        },
+        { status: 404 }
+      );
+    }
+
+    if (subs.subscriptionType !== "free" && subs.status !== "activated") {
+      return c.json(
+        {
+          message: "⚠️ Warning",
+          category: null,
+          description: "Your subscription has expired. Please re-subscribe to continue.",
+          file: null,
+        },
+        { status: 400 }
+      );
+    }
+
+    if (subs.selectedStorage <= subs.usedStorage) {
+      return c.json(
+        {
+          message: "⚠️ Warning",
+          category: null,
+          description: "Storage limit exceeded. Please subscribe and select additional storage.",
+          file: null,
+        },
+        { status: 400 }
+      );
+    }
+    // Kategorisasi File, periksa fortmat file dengan `getCategoryFromMimeType` for detail
+    const category = data.get("category") as string;
+    console.log("Received category:", category);
+    if (!["bmkg-station", "citra-satelit", "temperatur-laut", "daily-weather"].includes(category)) {
+      return c.json({ message: "Invalid category" }, { status: 400 });
+    }
+    console.log("Received category:", category);
+    const categoryMap: Record<string, string> = {
+      "bmkg-station": "BMKG",
+      "citra-satelit": "Citra Satelit",
+      "temperatur-laut": "Temperatur Laut",
+      "daily-weather": "Daily Weather",
+    };
+
+    const mappedCategory = categoryMap[category] || category;
+    // unggah File ke Pinata
+    // **Upload file ke Pinata hanya setelah semua validasi berhasil**
+    const uploadData = await pinata.upload.file(file).addMetadata({
+      keyvalues: { userId, name },
+    });
+
+    // Menyimpan Informasi File ke Database
+    const uploadedFile = await File.create({
+      pinataId: uploadData.id,
+      name: uploadData.name,
+      mimeType: uploadData.mime_type,
+      cid: uploadData.cid,
+      size: uploadData.size,
+      userInfo: { id: userId, name },
+      category: mappedCategory,
+    });
+
+    await Subscription.updateOne(
+      { subscriber: userId },
+      {
+        $inc: {
+          usedStorage: uploadData.size,
+        },
+      }
+    );
+
+    return c.json(
+      {
+        message: "Upload Successful",
+        category,
+        description: `File: ${uploadData.name}`,
+        file: uploadedFile,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log("Error in file uploading: ", error);
+
+    const err = parseError(error);
+
+    return c.json({ message: "Error", description: err, file: null }, { status: 500 });
+  }
+});
+
 
 // endpoint untuk membuat halaman khushs untuk image | doc | pdf dll
 // fileRoute.get("/:page", async (c) => {
@@ -241,152 +370,9 @@ fileRoute.get("/:category", async (c) => {
   }
 });
 
-fileRoute.post("/upload", async (c) => {
-  try {
-    // pastikan koneksi dengan database
-    await db();
 
-    const data = await c.req.formData();
-    // const file: File | null = data.get("file") as unknown as File;
-    // Ambil file dari request (data.get("file")).
-    const file = data.get("file") as File | null;
-    if (!file) {
-      return c.json({ message: "No file provided" }, { status: 400 });
-    }
-    console.log("Received file:", file);
-    console.log("File type:", file?.type);
 
-    const session = await getServerSession();
 
-    if (!session) {
-      return c.json(
-        {
-          file: null,
-          message: "Unauthorized",
-          description: "You need to be logged in to upload files",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
 
-    const userId = session.user.id;
-    const name = session.user.name;
-
-    const subs = await Subscription.findOne({ subscriber: userId });
-
-    if (!subs) {
-      return c.json(
-        {
-          message: "⚠️ Warning",
-          category: null,
-          description: "Subscription not found. Please log out and log in again to refresh your session.",
-          file: null,
-        },
-        { status: 404 }
-      );
-    }
-
-    if (subs.subscriptionType !== "free" && subs.status !== "activated") {
-      return c.json(
-        {
-          message: "⚠️ Warning",
-          category: null,
-          description: "Your subscription has expired. Please re-subscribe to continue.",
-          file: null,
-        },
-        { status: 400 }
-      );
-    }
-
-    if (subs.selectedStorage <= subs.usedStorage) {
-      return c.json(
-        {
-          message: "⚠️ Warning",
-          category: null,
-          description: "Storage limit exceeded. Please subscribe and select additional storage.",
-          file: null,
-        },
-        { status: 400 }
-      );
-    }
-    // Kategorisasi File, periksa fortmat file dengan `getCategoryFromMimeType` for detail
-    const category = data.get("category") as string;
-    console.log("Received category:", category);
-    if (!["bmkg-station", "citra-satelit", "temperatur-laut", "daily-weather"].includes(category)) {
-      return c.json({ message: "Invalid category" }, { status: 400 });
-    }
-    console.log("Received category:", category);
-    const categoryMap: Record<string, string> = {
-      "bmkg-station": "BMKG",
-      "citra-satelit": "Citra Satelit",
-      "temperatur-laut": "Temperatur Laut",
-      "daily-weather": "Daily Weather",
-    };
-
-    const mappedCategory = categoryMap[category] || category;
-    // unggah File ke Pinata
-    // **Upload file ke Pinata hanya setelah semua validasi berhasil**
-    const uploadData = await pinata.upload.file(file).addMetadata({
-      keyvalues: { userId, name },
-    });
-
-    // Menyimpan Informasi File ke Database
-    const uploadedFile = await File.create({
-      pinataId: uploadData.id,
-      name: uploadData.name,
-      mimeType: uploadData.mime_type,
-      cid: uploadData.cid,
-      size: uploadData.size,
-      userInfo: { id: userId, name },
-      category: mappedCategory,
-    });
-
-    await Subscription.updateOne(
-      { subscriber: userId },
-      {
-        $inc: {
-          usedStorage: uploadData.size,
-        },
-      }
-    );
-
-    return c.json(
-      {
-        message: "Upload Successful",
-        category,
-        description: `File: ${uploadData.name}`,
-        file: uploadedFile,
-      },
-      { status: 201 }
-    );
-  } catch (error) {
-    console.log("Error in file uploading: ", error);
-
-    const err = parseError(error);
-
-    return c.json({ message: "Error", description: err, file: null }, { status: 500 });
-  }
-});
-
-//==================== TES RECENT 2 :
-fileRoute.get("/recent2", async (c) => {
-  try {
-    console.log("=== Endpoint /recent2 terpanggil ===");
-    await db();
-
-    const recentFiles = await File.find().sort({ createdAt: -1 }).limit(10);
-    console.log("=== Data yang dikirim ke frontend ===");
-    console.log(recentFiles); // Debugging
-
-    return c.json({ message: "Success", data: { files: recentFiles, total: recentFiles.length } }); // Pastikan selalu mengembalikan array
-  } catch (error) {
-    console.error("Error fetching recent files:", error);
-    return c.json({ files: [], message: "Error", description: "Failed to fetch recent files" }, { status: 500 });
-  }
-});
-
-// =================== RECENT ENDPOINT GAGAL
 
 export default fileRoute;
