ea11b6aa7f1e3c9f4158065965f3fb9e0d8daea1
dzulkiram hilmi <bukutulis0110@gmail.com>
2025-02-27 11:28:17 +0700

    fix: correct category matching in visualization route

--- src/app/api/v1/[[...route]]/route.ts
+++ src/app/api/v1/[[...route]]/route.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import fileRoute from '../_routes/file.route'
 import paddleRoute from '../_routes/paddle.route'
-
+import visualizationRoute from '../_routes/visualization.route'
 export const runtime = 'nodejs'
 
 const app = new Hono().basePath('/api/v1')
@@ -10,6 +10,8 @@ const app = new Hono().basePath('/api/v1')
 app.route("/files", fileRoute)
 
 app.route("/webhook", paddleRoute)
+
+app.route("/visualization", visualizationRoute);
 
 export const GET = handle(app)
 export const POST = handle(app)
