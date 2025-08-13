50d32c3ed3f909f58cbed69f89f55672737f38f2
kiramDz <hilmi0@mhs.unsyiah.ac.id>
2025-08-13 11:11:57 +0700

    feat: implement login button when user session is not available

--- apps/nextjs/src/app/dashboard/_components/header-profile.tsx
+++ apps/nextjs/src/app/dashboard/_components/header-profile.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { signOut, useSession } from "@/lib/better-auth/auth-client";
 import { useRouter } from "next/navigation";
 import Avatar from "boring-avatars";
-// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 const HeaderProfile = () => {
@@ -16,18 +16,18 @@ const HeaderProfile = () => {
     return <Skeleton className="size-10 rounded-full" />;
   }
 
-  // if (!data?.user) {
-  //   return (
-  //     <Button variant="outline" onClick={() => router.push("/sign-in")}>
-  //       Login
-  //     </Button>
-  //   );
-  // }
+  if (!data?.user) {
+    return (
+      <Button variant="outline" onClick={() => router.push("/sign-in")}>
+        Login
+      </Button>
+    );
+  }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <Avatar name={data?.user.name || "User"} colors={["#0a0310", "#49007e", "#ff005b", "#ff7d10", "#ffb238"]} variant="sunset" size={40} />
+        <Avatar name={data.user.name || "User"} colors={["#0a0310", "#49007e", "#ff005b", "#ff7d10", "#ffb238"]} variant="sunset" size={40} />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="px-2">
         <DropdownMenuLabel>Action</DropdownMenuLabel>
